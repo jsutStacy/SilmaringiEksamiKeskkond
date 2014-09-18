cdbfe2b3878db001fb67dd3cdfa1ac79b47c2b28
martinappo <martinappo@gmail.com>
2014-09-18 22:13:18 +0300

    Comment added

--- apps/randomthing/models.py
+++ apps/randomthing/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+"""test"""
 class Person(models.Model):
     first_name = models.CharField(blank=True, max_length=20)
     family_name = models.CharField(max_length=50)
