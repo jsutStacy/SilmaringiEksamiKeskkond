c8f3fcaf6df30ce9007fa5b7feb786f1412eda48
andrestaht <andres.taht@gmail.com>
2014-09-24 22:51:37 +0300

    Deleted zend gitignore files and commited some local configs

---
+++ config/autoload/database.local.php
@@ -0,0 +1,22 @@
+<?php
+
+    return array(
+        'db' => array(
+            'driver' => 'Pdo',
+            'dsn' => 'mysql:dbname=silmaring;host=localhost',
+        ),
+        'service_manager' => array(
+            'factories' => array(
+                'Zend\Db\Adapter\Adapter' => function ($serviceManager) {
+                    $adapterFactory = new Zend\Db\Adapter\AdapterServiceFactory();
+                    $adapter = $adapterFactory->createService($serviceManager);
+    
+                    \Zend\Db\TableGateway\Feature\GlobalAdapterFeature::setStaticAdapter($adapter);
+    
+                    return $adapter;
+                }
+            ),
+        ),
+    );
+
+?>
