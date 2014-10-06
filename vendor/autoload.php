59596571fe5102db5b233499ab9cfb26c90b4910
martinappo <martinappo@gmail.com>
2014-10-06 22:40:30 +0300

    Zend

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit1a6fdd05d3a73ae3d18d660683c33c1e::getLoader();
+return ComposerAutoloaderInit962a9420a1961b62dd87fdb277d919f1::getLoader();
