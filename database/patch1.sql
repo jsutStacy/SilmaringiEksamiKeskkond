2ba5dcd6bb4046b5d962a6377dff033ea2f06bd6
martinappo <martinappo@gmail.com>
2014-11-25 15:09:07 +0200

    Database patch 1.

---
+++ database/patch1.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS `subsubject` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `subject_id` int(10) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+ALTER TABLE  `subsubject` ADD CONSTRAINT  `fk_subject_to_subsubject` FOREIGN KEY (  `subject_id` ) REFERENCES  `silmaring`.`subject` (
+`id`
+) ON DELETE CASCADE ON UPDATE RESTRICT ;
+
+ALTER TABLE  `lesson` CHANGE  `subject_id`  `subsubject_id` INT( 10 ) NULL DEFAULT NULL ;
+
+ALTER TABLE  `lesson` DROP FOREIGN KEY  `fk_subject_to_lesson` ;
+
+ALTER TABLE  `lesson` ADD CONSTRAINT  `fk_subsubject_to_lesson` FOREIGN KEY (  `subsubject_id` ) REFERENCES  `silmaring`.`subsubject` (
+`id`
+) ON DELETE CASCADE ON UPDATE RESTRICT ;
+
+ALTER TABLE  `lesson` CHANGE  `name`  `name` VARCHAR( 255 ) NULL DEFAULT NULL ;
