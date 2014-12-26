2a288f6bd78c22bb977e1976e1c376873bfd342b
martinappo <martinappo@gmail.com>
2014-12-26 18:17:31 +0200

    integration-rar

--- integration/Silmaring/config/autoload/db.live.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'db' => array(
-        'driver'    => 'pdo',
-        'dsn'       => 'mysql:dbname=silmaring_2;host=localhost',
-        'username'  => '',
-        'password'  => '',
-    )
-);
--- integration/Silmaring/module/Application/language/translations.php
+++
@@ -1,5 +0,0 @@
-<?php
-_e('Schools');
-_e('Users');
-_e('The input is an empty string');
-_e('File was not uploaded');
